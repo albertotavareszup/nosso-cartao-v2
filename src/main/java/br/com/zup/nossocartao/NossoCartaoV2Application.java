c761996c987906d715225f2011a005fc94dc41f3
alberto souza <alots.ssa@gmail.com>
2020-08-04 15:55:07 -0300

    setup inicial do projeto

---
+++ src/main/java/br/com/zup/nossocartao/NossoCartaoV2Application.java
@@ -0,0 +1,13 @@
+package br.com.zup.nossocartao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoCartaoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoCartaoV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/nossocartao/NossoCartaoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.nossocartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoCartaoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
