30fcd8ad02315eeb3c7cc668f263d8f5321241e0
alberto souza <alots.ssa@gmail.com>
2020-08-07 07:27:04 -0300

    [bloqueia documento igual] handler para tratar status

---
+++ src/main/java/br/com/zup/nossocartao/compartilhado/FieldErrorOutputDto.java
@@ -0,0 +1,21 @@
+package br.com.zup.nossocartao.compartilhado;
+public class FieldErrorOutputDto {
+
+    private String field;
+    private String message;
+
+    FieldErrorOutputDto() { }
+
+    public FieldErrorOutputDto(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
