e4cab91ab4c342e89139123adcb9bc28eae6769b
alberto souza <alots.ssa@gmail.com>
2020-08-12 06:26:48 -0300

    [avalia proposta] 8 pontos de complexidade. Refatora

--- src/main/java/br/com/zup/nossocartao/novaproposta/CriaNovaPropostaController.java
+++ src/main/java/br/com/zup/nossocartao/novaproposta/CriaNovaPropostaController.java
@@ -3,7 +3,6 @@ package br.com.zup.nossocartao.novaproposta;
 import java.net.URI;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -17,12 +16,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 import br.com.zup.nossocartao.outrossistemas.Integracoes;
 import br.com.zup.nossocartao.outrossistemas.NovoDocumentoRequest;
 
+//8 pontos de complexidade
 @RestController
 public class CriaNovaPropostaController {
 
 	private EntityManager manager;
+	//1
 	private BloqueiaDocumentoIgualValidator bloqueiaDocumentoIgualValidator;
+	//1
 	private Integracoes integracoes;
+	//1
 	private ExecutorTransacao executorTransacao;
 
 	public CriaNovaPropostaController(EntityManager manager,
@@ -38,18 +41,23 @@ public class CriaNovaPropostaController {
 
 	@PostMapping(value = "/propostas")
 	public ResponseEntity<?> cria(
+			//1
 			@RequestBody @Valid NovaPropostaRequest request,
 			UriComponentsBuilder builder) {
+		//1
 		if (!bloqueiaDocumentoIgualValidator.estaValido(request)) {
 			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
 		}
 
+		//1
 		Proposta novaProposta = request.toModel();
 		executorTransacao.salvaEComita(novaProposta);
 
+		//1
 		String resultadoAvaliacao = integracoes
 				.avalia(new NovoDocumentoRequest(novaProposta));
 
+		//1 por conta do retorno da enum nova
 		novaProposta.atualizaStatus(RespostaStatusAvaliacao
 				.valueOf(resultadoAvaliacao).getStatusAvaliacao());
 
