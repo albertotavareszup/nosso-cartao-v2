fa34c495ac089007d267a4bec65cca8a04ae4e55
alberto souza <albertotavares@zup.com.br>
2020-08-22 07:03:09 -0300

    [associa cartao e proposta] deixando o codigo direto no scheduler

--- src/main/java/br/com/zup/nossocartao/associacartao/AssociaPropostaCartaoScheduler.java
+++ src/main/java/br/com/zup/nossocartao/associacartao/AssociaPropostaCartaoScheduler.java
@@ -1,19 +1,44 @@
 package br.com.zup.nossocartao.associacartao;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
-import br.com.zup.nossocartao.outrossistemas.IntegracoesInternas;
+import br.com.zup.nossocartao.novaproposta.ExecutorTransacao;
+import br.com.zup.nossocartao.novaproposta.Proposta;
+import br.com.zup.nossocartao.novaproposta.StatusAvaliacaoProposta;
+import br.com.zup.nossocartao.outrossistemas.DocumentoProposta;
+import br.com.zup.nossocartao.outrossistemas.Integracoes;
 
 @Controller
 public class AssociaPropostaCartaoScheduler {
 	
 	@Autowired
-	private IntegracoesInternas integracoesInternas;
+	private Integracoes integracoes;
+	@Autowired
+	private ExecutorTransacao executorTransacao;
+	@Autowired
+	private PropostaRepository propostaRepository;
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(AssociaPropostaCartaoScheduler.class);
+
 
 	@Scheduled(fixedDelayString = "${periodicidade.associa-proposta-cartao}")
 	public void associa() {
-		integracoesInternas.associaPropostasECartoes();
-	}
+		List<Proposta> propostas = propostaRepository.todasSemCartao(StatusAvaliacaoProposta.elegivel);
+		
+		log.info("Existem {} propostas para avaliar",propostas.size());
+		
+		for (Proposta proposta : propostas) {
+			String numero = integracoes.buscaNumeroCartao(new DocumentoProposta(proposta.getDocumento()));
+			proposta.associaCartao(numero);
+			executorTransacao.atualizaEComita(proposta);
+			log.info("Proposta [{}] teve cartão associada",proposta.getId());
+		}
+	}	
 }
